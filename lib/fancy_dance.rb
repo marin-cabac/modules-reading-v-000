221538c3b8209e7295b9c5d1add55c4eb344a250
Marin Cabac <marin.cabac@gmail.com>
2018-03-13 19:57:55 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -17,4 +17,10 @@ module FancyDance
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
-end 
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+end
